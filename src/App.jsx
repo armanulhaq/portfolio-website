888eeee5c6563d9fc5a80886c3771e177352f951
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-12-16 13:41:00 +0530

    add animation in skill section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
@@ -13,6 +14,7 @@ export default function App() {
             <About />
             <Skills />
             <Projects />
+            <Contact />
         </div>
     );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -22,79 +22,87 @@ const Skills = () => {
                     Skills
                 </div>
             </div>
-            <div></div>
-            <div className="my-4 lg:my-10 h-[90vh] lg:h-[60vh] p-4 lg:p-10 grid grid-cols-3 lg:grid-cols-4 flex-wrap gap-2 lg:gap-5 items-center justify-center border border-gray-600 rounded-3xl mx-8 lg:mx-60 bg-black text-white">
-                <div className="flex flex-col items-center text-center">
-                    <div className="text-4xl">
-                        <img className="h-10 w-10" src={html} alt="" />
+            <div className="flex flex-col lg:flex-row items-center justify-center h-full ">
+                <div className="w-[90%] lg:ml-[130px] lg:w-[60%] my-4 lg:my-10 h-[60vh]  lg:p-10 grid grid-cols-3 lg:grid-cols-4 flex-wrap gap-2 lg:gap-5 items-center justify-center border border-gray-600 rounded-3xl  bg-black text-white">
+                    <div className="flex flex-col items-center text-center">
+                        <div className="text-4xl">
+                            <img className="h-10 w-10" src={html} alt="" />
+                        </div>
+                        <h3 className="font-bold text-lg mt-2">HTML</h3>
                     </div>
-                    <h3 className="font-bold text-lg mt-2">HTML</h3>
+                    <div className="flex flex-col items-center text-center">
+                        <div className="text-4xl">
+                            <img className="h-10 w-10" src={css} alt="" />
+                        </div>
+                        <h3 className="font-bold text-lg mt-2">CSS</h3>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <div className="text-4xl">
+                            <img className="h-10 w-10" src={js} alt="" />
+                        </div>
+                        <h3 className="font-bold text-lg mt-2">JavaScript</h3>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <div className="text-4xl">
+                            <img className="h-10 w-10" src={bootstrap} alt="" />
+                        </div>
+                        <h3 className="font-bold text-lg mt-2">Bootstrap</h3>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <div className="text-4xl">
+                            <img className="h-10 w-10" src={java} alt="" />
+                        </div>
+                        <h3 className="font-bold text-lg mt-2">Java</h3>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <div className="text-4xl">
+                            <img className="h-10 w-10" src={mysql} alt="" />
+                        </div>
+                        <h3 className="font-bold text-lg mt-2">MySQL</h3>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <div className="text-4xl">
+                            <img className="h-10 w-10" src={react} alt="" />
+                        </div>
+                        <h3 className="font-bold text-lg mt-2">React.js</h3>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <div className="text-4xl">
+                            <img className="h-10 w-10" src={shadcn} alt="" />
+                        </div>
+                        <h3 className="font-bold text-lg mt-2">shadcn/ui</h3>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <div className="text-4xl">
+                            <img className="h-10 w-10" src={tailwind} alt="" />
+                        </div>
+                        <h3 className="font-bold text-lg mt-2">Tailwind</h3>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <div className="text-4xl">
+                            <img className="h-10 w-10" src={git} alt="" />
+                        </div>
+                        <h3 className="font-bold text-lg mt-2">GIT</h3>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <div className="text-4xl">
+                            <img className="h-10 w-10" src={figma} alt="" />
+                        </div>
+                        <h3 className="font-bold text-lg mt-2">Figma</h3>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <div className="text-4xl">
+                            <img className="h-10 w-10" src={python} alt="" />
+                        </div>
+                        <h3 className="font-bold text-lg mt-2">Python</h3>
+                    </div>
                 </div>
-                <div className="flex flex-col items-center text-center">
-                    <div className="text-4xl">
-                        <img className="h-10 w-10" src={css} alt="" />
-                    </div>
-                    <h3 className="font-bold text-lg mt-2">CSS</h3>
-                </div>
-                <div className="flex flex-col items-center text-center">
-                    <div className="text-4xl">
-                        <img className="h-10 w-10" src={js} alt="" />
-                    </div>
-                    <h3 className="font-bold text-lg mt-2">JavaScript</h3>
-                </div>
-                <div className="flex flex-col items-center text-center">
-                    <div className="text-4xl">
-                        <img className="h-10 w-10" src={bootstrap} alt="" />
-                    </div>
-                    <h3 className="font-bold text-lg mt-2">Bootstrap</h3>
-                </div>
-                <div className="flex flex-col items-center text-center">
-                    <div className="text-4xl">
-                        <img className="h-10 w-10" src={java} alt="" />
-                    </div>
-                    <h3 className="font-bold text-lg mt-2">Java</h3>
-                </div>
-                <div className="flex flex-col items-center text-center">
-                    <div className="text-4xl">
-                        <img className="h-10 w-10" src={mysql} alt="" />
-                    </div>
-                    <h3 className="font-bold text-lg mt-2">MySQL</h3>
-                </div>
-                <div className="flex flex-col items-center text-center">
-                    <div className="text-4xl">
-                        <img className="h-10 w-10" src={react} alt="" />
-                    </div>
-                    <h3 className="font-bold text-lg mt-2">React.js</h3>
-                </div>
-                <div className="flex flex-col items-center text-center">
-                    <div className="text-4xl">
-                        <img className="h-10 w-10" src={shadcn} alt="" />
-                    </div>
-                    <h3 className="font-bold text-lg mt-2">shadcn/ui</h3>
-                </div>
-                <div className="flex flex-col items-center text-center">
-                    <div className="text-4xl">
-                        <img className="h-10 w-10" src={tailwind} alt="" />
-                    </div>
-                    <h3 className="font-bold text-lg mt-2">Tailwind</h3>
-                </div>
-                <div className="flex flex-col items-center text-center">
-                    <div className="text-4xl">
-                        <img className="h-10 w-10" src={git} alt="" />
-                    </div>
-                    <h3 className="font-bold text-lg mt-2">GIT</h3>
-                </div>
-                <div className="flex flex-col items-center text-center">
-                    <div className="text-4xl">
-                        <img className="h-10 w-10" src={figma} alt="" />
-                    </div>
-                    <h3 className="font-bold text-lg mt-2">Figma</h3>
-                </div>
-                <div className="flex flex-col items-center text-center">
-                    <div className="text-4xl">
-                        <img className="h-10 w-10" src={python} alt="" />
-                    </div>
-                    <h3 className="font-bold text-lg mt-2">Python</h3>
+                <div className="w-[40%] lg:flex items-end justify-end mr-[130px] hidden">
+                    <img
+                        src="https://cdn.dribbble.com/users/4055494/screenshots/15215756/media/d2b66c4ca0192aa26d103448b3d1518b.gif"
+                        alt=""
+                        className="rounded-3xl h-[400px]"
+                    />
                 </div>
             </div>
         </div>
